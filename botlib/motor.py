0ab2a924b02716e89d489c1f0e16d6f39c2b0d2f
witling <noreply@my.email>
2020-01-23 20:26:37 +0000

    add power limitation to motor

--- botlib/motor.py
+++ botlib/motor.py
@@ -3,32 +3,32 @@ from .brickpi3 import *
 class Motor:
     _bp = BrickPi3()
 
-    # TODO: call set_motor_limits for limiting power
     def __init__(self, port, pmin=None, pmax=None):
         self._port = port
-        self._pmin = pmin
-        self._pmax = pmax
-        self._pinit = None
+
+        # TODO: remove this limitation once we have proper power management
+        self._bp.set_motor_limits(self._port, 85)
 
     def status(self):
         return self._bp.get_motor_status(self._port)
 
     # gracefully morph current power to pnew
     def change_power(self, pnew):
-        #if (self._pmin and self._pmax) and not (self._pmin <= pnew <= self._pmax):
-            #raise Exception('power {} is out of bounds for motor {}'.format(pnew, self._port))
         self._bp.set_motor_power(self._port, pnew)
 
     def stop(self):
         self.change_power(0)
 
 class CalibratedMotor(Motor):
-    # TODO: call set_motor_limits for limiting power
     def __init__(self, port, pmin=None, pmax=None, calpow=20):
         super().__init__(port, pmin, pmax)
 
         # power with which the motor will be calibrated
         self._calpow = calpow
+        # minimum position
+        self._pmin = pmin
+        # maximum position
+        self._pmax = pmax
         # initial position for this motor. will be determined in `calibrate`
         self._pinit = None
 
