2d014e6b6de47106435057667c561567a163648b
witling <noreply@my.email>
2020-01-24 12:38:36 +0100

    rename project directory

--- examples/drive-test.py
+++ examples/drive-test.py
@@ -1,8 +1,9 @@
-import brickpi3
 import time
 
-BP = brickpi3.BrickPi3()
-BP.set_motor_power(BP.PORT_B, 20)
-time.sleep(1)
-BP.set_motor_power(BP.PORT_B, 0)
+if __name__ == '__main__':
+    print(dir(botlib))
+    print(botlib.__file__)
+    Motor._bp.set_motor_power(BP.PORT_B, 20)
+    time.sleep(1)
+    Motor._bp.set_motor_power(BP.PORT_B, 0)
 
