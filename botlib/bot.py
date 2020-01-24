5ccc693ec51bba6dbf95132216863005393d9baa
witling <noreply@my.email>
2020-01-24 14:04:14 +0100

    add empty Camera class

--- botlib/bot.py
+++ botlib/bot.py
@@ -1,12 +1,13 @@
-from .motor import Motor
+from .camera import Camera
 from .forklift import Forklift
+from .motor import Motor
 
 class Bot:
     def __init__(self):
         self._drive_motor = Motor(Motor._bp.PORT_B)
         self._steer_motor = Motor(Motor._bp.PORT_D)
 
-        self._bp = brickpi3.BrickPi3()
+        self._camera = Camera(self)
         self._forklift = Forklift(self)
     
     def calibrate(self):
---
+++ botlib/camera.py
@@ -0,0 +1,3 @@
+class Camera:
+    def __init__(self, bot):
+        self._bot = bot
