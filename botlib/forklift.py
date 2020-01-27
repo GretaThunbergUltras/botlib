f3dadced99ddf2c000b9ee41dff31bb32d87af11
witling <laurin.kirbach@wuerth-it.com>
2020-01-27 12:03:14 +0000

    experimental fork rotation

--- botlib/forklift.py
+++ botlib/forklift.py
@@ -16,16 +16,23 @@ class Forklift:
 
     def calibrate(self):
         # TODO: standard calibration routine does not work well with this one
-        #self._rotate_motor.calibrate()
+        # self._rotate_motor.calibrate()
+        self._rotate_motor._pmin = self._rotate_motor._pinit = -128
+        self._rotate_motor._pmax = 15603
+
         self._height_motor.calibrate()
 
     def to_carry_mode(self):
         # rotate backwards
+        self._rotate_motor.change_position(self._rotate_motor._pmax)
+
         # move fork up
         self._height_motor.to_init_position()
 
     def to_pickup_mode(self):
         # rotate forward
+        self._rotate_motor.to_init_position()
+
         # move fork down
         pos = self._height_motor.position_from_factor(-1.0)
         self._height_motor.change_position(pos)
