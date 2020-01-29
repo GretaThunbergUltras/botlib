34e93134888971f9487d49df112eba3d11af2f81
l.werberich <Louis.Werberich@flyeralarm.com>
2020-01-29 13:35:05 +0100

    added support for gamepads

--- examples/remote.py
+++ examples/remote.py
@@ -8,6 +8,7 @@ def main():
 
     from botlib.bot import Bot
     from readchar import readkey, key
+    from inputs import devices, get_gamepad
 
     bot = Bot()
     STEP_POWER, STEP_STEER = 10, 0.25
@@ -24,6 +25,33 @@ def main():
     print('Up/Down: manage speed, Left/Right: manage direction, w/s: Carry/Pickup, Space: stop, Backspace: exit')
 
     while running:
+        if devices.gamepads:
+            events = get_gamepad()
+            for event in events:
+                # print(event.ev_type, event.code, event.state)
+                if event.code == "ABS_RZ":
+                    power = round(event.state / 10.23, 0)
+                if event.code == "ABS_RY":
+                    if (event.state >= 0 < power) or (event.state < 0 > power):
+                        power = power * (-1)
+                    # power = round(event.state/327.67, 0)
+                    bot.drive_power(power)
+                    # print("power: " + str(power))
+                if event.code == "ABS_X":
+                    bot.drive_steer(round(event.state / 32767, 2))
+                    # print("steering:" + str(round(event.state / 32767, 2)))
+                if event.code == "ABS_HAT0X":
+                    if event.state == 1:
+                        bot._forklift.to_pickup_mode()
+                    else:
+                        bot._forklift.to_carry_mode()
+                """if event.code == "ABS_HAT0Y":
+                    if event.state == -1:
+                        print("Forklift up")
+                    else:
+                        print("Forklift down")"""
+
+
         inp = readkey()
         if inp == key.DOWN or inp == key.UP:
             power += STEP_POWER if inp == key.UP else -STEP_POWER
@@ -47,7 +75,7 @@ def main():
 
     if '--camera' in sys.argv:
         bot._camera.stop()
-        
+
     print('bye...')
 
 if __name__ == '__main__':
