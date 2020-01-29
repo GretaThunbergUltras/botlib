e3dcc4c5b9366a7727308afb434afaf26ae28563
l.werberich <Louis.Werberich@flyeralarm.com>
2020-01-29 13:43:46 +0100

    added support for gamepads

--- examples/remote.py
+++ examples/remote.py
@@ -2,6 +2,7 @@
 
 from botlib.bot import Bot
 from readchar import readkey, key
+from inputs import devices, get_gamepad
 
 PORT = 6666
 
@@ -62,7 +63,32 @@ def handle_event(inp):
     global bot
     global power
     global steer
-
+    if devices.gamepads:
+        events = get_gamepad()
+        for event in events:
+            # print(event.ev_type, event.code, event.state)
+            if event.code == "ABS_RZ":
+                power = round(event.state / 10.23, 0)
+            if event.code == "ABS_RY":
+                if (event.state >= 0 < power) or (event.state < 0 > power):
+                    power = power * (-1)
+                # power = round(event.state/327.67, 0)
+                bot.drive_power(power)
+                # print("power: " + str(power))
+            if event.code == "ABS_X":
+                steer = round(event.state / 32767, 2)
+                bot.drive_steer(steer)
+                # print("steering:" + str(round(event.state / 32767, 2)))
+            if event.code == "ABS_HAT0X":
+                if event.state == 1:
+                    bot._forklift.to_pickup_mode()
+                else:
+                    bot._forklift.to_carry_mode()
+            """if event.code == "ABS_HAT0Y":
+                if event.state == -1:
+                    print("Forklift up")
+                else:
+                    print("Forklift down")"""
     STEP_POWER, STEP_STEER = 10, 0.25
     return
 
