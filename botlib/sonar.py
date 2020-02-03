1fcf5f9a495ad5bc90acca006bde1fdd8f80e4e0
witling <laurin.kirbach@wuerth-it.com>
2020-02-03 09:43:06 +0100

    handle read errors for sonar

--- botlib/sonar.py
+++ botlib/sonar.py
@@ -20,16 +20,35 @@ class Sonic:
         self._sonic.measure.restype = ctypes.c_double
         self._sonic.initialize()
 
+    def _try_read(self, sensor, times=3):
+        for _ in range(times):
+            ret = self.read(sensor)
+            if ret != None:
+                return ret
+        return None
+
     def read(self, sensor: int):
-        return self._sonic.measure(ctypes.c_uint(sensor))
+        """
+        Read try to read a sensor value
+
+        :param sensor: the id of the sensor.
+        :return: the read distance (rounded) or `None` if read failed
+        """
+        ret = self._sonic.measure(ctypes.c_uint(sensor))
+        return None if ret == 0 else round(ret, 2)
 
     def read_all(self):
+        """
+        Read all sensors.
+
+        :return: an array having `self._sensor_count` items. if error occurred on 
+        sensor, the item is `None`.
+        """
         results = []
         for i in range(self._sensor_count):
             ret = self.read(i)
-            if ret == 0:
-                i -= 1
-            else:
-                results.append(round(ret, 2))
+            if ret == None:
+                ret = self._try_read(i)
+            results.append(ret)
         return results
 
